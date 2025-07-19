0de98b8d52b69d68355db71fb8ed540a8420c7b6
P.A.J.U.Pallewela <janithup10@gmail.com>
2025-07-19 13:57:56 +0530

    make cart modal

---
+++ src/store/UserProgressContext.jsx
@@ -0,0 +1,13 @@
+
+import { createContext } from "react";
+
+const UserProgressContext = createContext({
+    progress: '' ,
+    showCart: ()=>{},
+    hideCart: ()=>{},
+    showCheckout: ()=>{},
+    hideCheckout: ()=>{},
+
+});
+
+
